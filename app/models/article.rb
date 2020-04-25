e7c5ffa2ec8ba3c43c9f2b0b2d83b9c6d09b2335
yutan0918 <inesperti_yutan@yahoo.co.jp>
2020-04-25 17:34:58 +0900

    Scaffolded Article Model with Rich Text & Thumbnail Image by Active Storage

---
+++ app/models/article.rb
@@ -0,0 +1,16 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 23 }
+  validate :image_content_type, if: :was_attached?
+  has_one_attached :thumbnail
+  has_rich_text :content
+
+  private
+    def image_content_type
+      extension = ['image/png', 'image/jpg', 'image/jpeg']
+      errors.add(:thumbnail, "の拡張子が間違っています。png、jpg、jpegのみ利用可能です") unless thumbnail.content_type.in?(extension)
+    end
+
+    def was_attached?
+      self.thumbnail.attached?
+    end
+end
